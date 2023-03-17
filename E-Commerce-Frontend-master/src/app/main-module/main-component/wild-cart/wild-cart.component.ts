b5f92f96b35d9c5259ac483205fd288fe6aa7deb
fazalshahzad <shancompany322@gmail.com>
2023-03-17 20:02:42 +0500

    v

--- E-Commerce-Frontend-master/src/app/main-module/main-component/wild-cart/wild-cart.component.ts
+++ E-Commerce-Frontend-master/src/app/main-module/main-component/wild-cart/wild-cart.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ProductInterface } from 'src/app/shared-service/productInterface/product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartInterface } from 'src/app/shared-service/CartInterface/cart-interface';
+import { CartService } from 'src/app/shared-service/CartSerice/cart.service';
+import { ToastrService } from 'ngx-toastr';
+import { LocalstorageService } from 'src/app/shared-service/saveDataLocalStorage/localstorage.service';
 
 
 
@@ -14,19 +17,27 @@ export class WildCartComponent implements OnInit {
   public imageIndex = "ProductImageUrl"
   public url = 'http://localhost:8686/'
   public getProductsId: any;
-  public getAllDataWithOwnId: object | ProductInterface | any = {}
-  public getAllDatafromProductService: any
+  public getAllDataWithOwnId: object | CartInterface | any = {}
+  public getAllDatafromCartService: any
   public selectedSizes: any = [];
   public totalQuantity: Number | any;
   public selectedQuantity: Number | any = 0;
   public offCanvasRight = '';
   public cart:any=[]
 
+  Url = 'http://localhost:8686/'
+
+
   constructor(private ActivatedRoute: ActivatedRoute,
+    private readonly getAllProductFrombackend: CartService,
+    private readonly Router: Router,
+    private readonly toaster: ToastrService,
+    private readonly loacalStorageData: LocalstorageService
 
   ) { }
 
   ngOnInit(): void {
+
 
   }
 
