55e8422b4ef74dcc1ed42252220db5a59a1e39a0
fazalshahzad <shancompany322@gmail.com>
2023-03-17 18:54:30 +0500

    commit

---
+++ E-Commerce-Frontend-master/src/app/shared-service/CartInterface/cart-interface.ts
@@ -0,0 +1,19 @@
+export interface CartInterface {
+
+  id:string;
+productName:string;
+quantity:number;
+price:number;
+companyName:string;
+color:string;
+size:[];
+description:string;
+ProductImages:[];
+category:string;
+softDelete:number;
+Status:number;
+CreateDate:string;
+createdAt:string;
+updatedAt:string;
+
+}
